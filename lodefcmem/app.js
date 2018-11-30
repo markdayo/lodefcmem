5a13dc28b1a86eb7f9b082dfcc29a0d12eec1271
markdayo <markdayo@gmail.com>
2018-11-30 18:37:01 +0900

    bug fix

--- lodefcmem/app.js
+++ lodefcmem/app.js
@@ -18,11 +18,11 @@ var lastPage = "999";
 var nextUrl = "";
 
 function getFCMembers(url) {
-
     client.fetch(url, param, function (err, $, res) {
         if (err) { console.log("Error:", err); return; }
-        // body•\Ž¦
+
         $(".ldst__main").each(function (idx) {
+            // console.log(nextPage);
 
             $(this).find('.btn__pager__next').each(function (id, el) {
                 //console.log($(this).attr("href"));
@@ -33,14 +33,15 @@ function getFCMembers(url) {
                 //console.log($(this).attr("href"));
                 lastPage = $(this).attr("href").slice(-1);
                 //console.log(lastPage);
-                if (nextPage != lastPage) {
-                    var nextUrl = "https://jp.finalfantasyxiv.com/lodestone/freecompany/" + targetFC + "/member/?page=" + nextPage
-                    var timerID = setTimeout(function () { getFCMembers(nextUrl); }, 10000);
-                }
             });
             $(this).find('.entry__name').each(function (id, el) {
                 console.log($(this).text());
             });
+
+            if (nextPage != ";" && lastPage != ";") {
+                var nextUrl = "https://jp.finalfantasyxiv.com/lodestone/freecompany/" + targetFC + "/member/?page=" + nextPage
+                var timerID = setTimeout(function () { getFCMembers(nextUrl); }, 10000);
+            }
         });
     });
 }
